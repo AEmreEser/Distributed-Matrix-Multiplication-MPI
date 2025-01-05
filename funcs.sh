47d1ca57cd5f00d2d1f667d3433da6c16f89254e
AEmreEser <emre.eser@sabanciuniv.edu>
2025-01-05 22:10:33 +0000

    run many tests func

--- funcs.sh
+++ funcs.sh
@@ -11,3 +11,15 @@ run() {
 comprun() {
 	compile $1 $2 -DBLOCK_SIZE=$3 && run $4 $2;
 }
+
+
+runtests() {
+	for j in {16..32..2}; do
+		echo ' ';
+		echo "*********** $j ***********"; 
+		echo ' ';
+		for i in {0..4}; do 
+			comprun $1 $2 $j 16; 
+		done; 
+	done
+}
