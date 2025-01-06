da00e787f9ea01bc2d66bb072e7f87a5f68443de
AEmreEser <emre.eser@sabanciuniv.edu>
2025-01-06 06:19:32 +0300

    optimizations and code reorg

--- blockedv1.cpp
+++ blockedv1.cpp
@@ -21,16 +21,9 @@ void init(double*& A, double*& B, double*& C) {
     B = (double*)aligned_alloc(64, N * N * sizeof(double));
     C = (double*)aligned_alloc(64, N * N * sizeof(double));
 
-    
-    for (unsigned j = 0; j < N; j++) {
-        for (unsigned i = 0; i < N; i++) {
-            B[j * N + i] = double(i * N + j) / N;
-        }
-    }
-    
-    
     for (unsigned i = 0; i < N * N; i++) {
-        A[i] = double(i) / N;
+        A[i] = double(i) / N; // double(std::rand() % 1000) / 10;
+        B[i] = double(i) / N; // double(std::rand() % 1000) / 10;
     }
 }
 
@@ -38,9 +31,7 @@ void init(double*& A, double*& B, double*& C) {
 void multiply_blocked(const double* A_local, const double* B, double* C_local, int my_rows) {
     vector<double> B_block(BLOCK_SIZE * BLOCK_SIZE);
     
-    
     fill_n(C_local, my_rows * N, 0.0);
-    
     
     for (int jj = 0; jj < N; jj += BLOCK_SIZE) {
         for (int kk = 0; kk < N; kk += BLOCK_SIZE) {
@@ -48,7 +39,7 @@ void multiply_blocked(const double* A_local, const double* B, double* C_local, i
             for (int i = 0; i < my_rows; i++) {
                 for (int j = jj; j < min(jj + BLOCK_SIZE, N); j++) {
                     double sum = C_local[i * N + j];
-                    #pragma unroll(8)  
+                    #pragma unroll(3) // old value: 8
                     for (int k = kk; k < min(kk + BLOCK_SIZE, N); k++) {
                         sum += A_local[i * N + k] * B[k * N + j];
                     }
@@ -65,19 +56,16 @@ int main() {
     int size, rank;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
     
     MPI_Datatype column_type;
     MPI_Type_vector(N, 1, N, MPI_DOUBLE, &column_type);
     MPI_Type_commit(&column_type);
 
-    
     int rows_per_proc = N / size;
     int remainder = N % size;
     int my_rows = (rank < remainder) ? rows_per_proc + 1 : rows_per_proc;
     int my_offset = rank * rows_per_proc + min(rank, remainder);
 
-    
     double* A_local = (double*)aligned_alloc(64, my_rows * N * sizeof(double));
     double* B = (double*)aligned_alloc(64, N * N * sizeof(double));
     double* C_local = (double*)aligned_alloc(64, my_rows * N * sizeof(double));
@@ -88,11 +76,9 @@ int main() {
     }
 
     double start = MPI_Wtime();
-
     
     MPI_Request bcast_request;
     MPI_Ibcast(B, N * N, MPI_DOUBLE, 0, MPI_COMM_WORLD, &bcast_request);
-
     
     vector<int> sendcounts(size);
     vector<int> displs(size);
@@ -100,25 +86,16 @@ int main() {
         sendcounts[i] = (i < remainder ? rows_per_proc + 1 : rows_per_proc) * N;
         displs[i] = (i * rows_per_proc + min(i, remainder)) * N;
     }
-
     
-    MPI_Scatterv(A, sendcounts.data(), displs.data(), MPI_DOUBLE,
-                 A_local, my_rows * N, MPI_DOUBLE,
-                 0, MPI_COMM_WORLD);
+    MPI_Scatterv(A, sendcounts.data(), displs.data(), MPI_DOUBLE, A_local, my_rows * N, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    
     MPI_Wait(&bcast_request, MPI_STATUS_IGNORE);
 
-    
     multiply_blocked(A_local, B, C_local, my_rows);
 
-    
     MPI_Request gather_request;
-    MPI_Igatherv(C_local, my_rows * N, MPI_DOUBLE,
-                 C, sendcounts.data(), displs.data(), MPI_DOUBLE,
-                 0, MPI_COMM_WORLD, &gather_request);
+    MPI_Igatherv(C_local, my_rows * N, MPI_DOUBLE, C, sendcounts.data(), displs.data(), MPI_DOUBLE, 0, MPI_COMM_WORLD, &gather_request);
 
-    
     MPI_Wait(&gather_request, MPI_STATUS_IGNORE);
 
     double end = MPI_Wtime();
@@ -131,7 +108,6 @@ int main() {
         cout << "Block Size: " << BLOCK_SIZE << "\n";
         cout << "Execution time: " << end - start << "\n";
 
-        
         double sum = 0.0;
         #pragma omp parallel for reduction(+:sum)
         for (unsigned i = 0; i < N * N; ++i) {
@@ -144,7 +120,6 @@ int main() {
         free(C);
     }
 
-    
     free(A_local);
     free(B);
     free(C_local);
